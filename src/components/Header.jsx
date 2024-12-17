482f6642febdb4a4b2a40c3f443ae0409a631f6c
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2024-12-17 20:14:11 +0100

    wip... navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
+                    <li><a href="#home">Blog</a></li>
+                    <li><a href="#about">Credit</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
