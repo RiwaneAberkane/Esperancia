6b8fe4072cc873c68d2d6f6a0f39721358dbaf9b
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2025-03-03 13:02:57 +0100

    wip ... NonProfit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import NonProfit from "./components/NonProfit";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <NonProfit />
       </header>
     </div>
   );
---
+++ src/components/NonProfit.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NonProfit.css';
+
+const NonProfit = () => {
+    return (
+        <div className="container">
+            <div className="headerNonProfit">
+                <h3 className="title">We are non-profit Charity & NGO Oranization</h3>
+            </div>
+            <div className="cards-wrapper">
+                <div className="cards-container">
+                    <div className="card">Carte 1</div>
+                    <div className="card">Carte 2</div>
+                    <div className="card">Carte 3</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NonProfit;
